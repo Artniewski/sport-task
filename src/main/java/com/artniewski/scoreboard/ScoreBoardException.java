a95b5bf130fcbc26034a99258b37ee3666c05965
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:07:12 +0200

    add test for name validation

--- src/main/java/com/artniewski/scoreboard/ScoreBoardException.java
+++
@@ -1,7 +0,0 @@
-package com.artniewski.scoreboard;
-
-public class ScoreBoardException extends RuntimeException {
-    public ScoreBoardException(String message) {
-        super(message);
-    }
-}

2b6079dfad6ee13a248ad51a9af4b2cb3dd5f08f
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:07:11 +0200

    implement starting game

--- src/main/java/com/artniewski/scoreboard/Match.java
+++ src/main/java/com/artniewski/scoreboard/Match.java
@@ -6,11 +6,11 @@ public class Match {
     private int homeScore;
     private int awayScore;
 
-    public Match(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+    public Match(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
-        this.homeScore = homeScore;
-        this.awayScore = awayScore;
+        this.homeScore = 0;
+        this.awayScore = 0;
     }
 
     public String getHomeTeam() {

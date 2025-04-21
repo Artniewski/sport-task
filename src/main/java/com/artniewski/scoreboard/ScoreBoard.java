c1941663c3e98fb0c0a8e5afb3084089306d8015
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:07:12 +0200

    refactor name validation

--- src/main/java/com/artniewski/scoreboard/ScoreBoard.java
+++ src/main/java/com/artniewski/scoreboard/ScoreBoard.java
@@ -1,13 +1,15 @@
 package com.artniewski.scoreboard;
 
 public class ScoreBoard {
-    public Match startGame(String homeTeam, String awayTeam) {
-        if (homeTeam == null || homeTeam.trim().isEmpty()) {
-            throw new IllegalArgumentException("Home team name cannot be null or empty");
+    public Match startGame(String homeTeamName, String awayTeamName) {
+        validate(homeTeamName);
+        validate(awayTeamName);
+        return new Match(homeTeamName, awayTeamName);
+    }
+
+    private static void validate(String homeTeamName) {
+        if (homeTeamName == null || homeTeamName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Team name cannot be null or empty");
         }
-        if (awayTeam == null || awayTeam.trim().isEmpty()) {
-            throw new IllegalArgumentException("Away team name cannot be null or empty");
-        }
-        return new Match(homeTeam, awayTeam);
     }
 }

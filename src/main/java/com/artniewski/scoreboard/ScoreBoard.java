e98fe9de6f364c828753172725ac1395ff4411ee
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:04:35 +0200

    refactor saving & validating matches

--- src/main/java/com/artniewski/scoreboard/ScoreBoard.java
+++ src/main/java/com/artniewski/scoreboard/ScoreBoard.java
@@ -9,31 +9,41 @@ public class ScoreBoard {
 
     public Match startGame(String homeTeamName, String awayTeamName) {
         validateTeamNames(homeTeamName, awayTeamName);
+        validateTeamsNotAlreadyPlaying(homeTeamName, awayTeamName);
+        return saveMatch(homeTeamName, awayTeamName);
+    }
+
+    private Match saveMatch(String homeTeamName, String awayTeamName) {
+        String matchId = composeMatchId(homeTeamName, awayTeamName);
         Match match = new Match(homeTeamName, awayTeamName);
-        matches.values()
-                .stream()
-                .filter(m -> m.getHomeTeam().equalsIgnoreCase(homeTeamName)
-                        || m.getAwayTeam().equalsIgnoreCase(homeTeamName)
-                        || m.getHomeTeam().equalsIgnoreCase(awayTeamName)
-                        || m.getAwayTeam().equalsIgnoreCase(awayTeamName))
-                .findFirst()
-                .ifPresent(m -> {
-                    throw new GameException("Cannot start a game with a team already in play");
-                });
-        String matchId = homeTeamName.toLowerCase() + "#" + awayTeamName.toLowerCase();
         matches.put(matchId, match);
         return match;
     }
 
+    private static String composeMatchId(String homeTeamName, String awayTeamName) {
+        return homeTeamName.toLowerCase() + "#" + awayTeamName.toLowerCase();
+    }
+
+    private void validateTeamsNotAlreadyPlaying(String homeTeamName, String awayTeamName) {
+        matches.values()
+                .stream()
+                .filter(m -> m.isInGame(homeTeamName) || m.isInGame(awayTeamName))
+                .findFirst()
+                .ifPresent(_ -> {
+                    throw new GameException("One of the teams is already playing");
+                });
+
+    }
+
     private static void validateTeamNames(String homeTeamName, String awayTeamName) {
-        validate(homeTeamName);
-        validate(awayTeamName);
+        validateTeamName(homeTeamName);
+        validateTeamName(awayTeamName);
         if (homeTeamName.equalsIgnoreCase(awayTeamName)) {
             throw new GameException("Home and away teams cannot be the same");
         }
     }
 
-    private static void validate(String homeTeamName) {
+    private static void validateTeamName(String homeTeamName) {
         if (homeTeamName == null || homeTeamName.isEmpty()) {
             throw new IllegalArgumentException("Team name cannot be null or empty");
         }

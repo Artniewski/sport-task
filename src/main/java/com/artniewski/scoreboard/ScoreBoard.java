bd1cd4998a46ae67b046e25198b72e72796dc104
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:07:46 +0200

    add test for name validation

--- src/main/java/com/artniewski/scoreboard/ScoreBoard.java
+++ src/main/java/com/artniewski/scoreboard/ScoreBoard.java
@@ -2,6 +2,7 @@ package com.artniewski.scoreboard;
 
 public class ScoreBoard {
     public Match startGame(String homeTeam, String awayTeam) {
+
         return new Match(homeTeam, awayTeam);
     }
 }

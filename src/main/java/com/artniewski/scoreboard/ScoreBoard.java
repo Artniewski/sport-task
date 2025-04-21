34c754b05233afe0c20544bcf9390d2e36be57ee
a.trzesniewski <artniewski@gmail.com>
2025-04-21 23:04:35 +0200

    implement storing started games

--- src/main/java/com/artniewski/scoreboard/ScoreBoard.java
+++ src/main/java/com/artniewski/scoreboard/ScoreBoard.java
@@ -1,10 +1,28 @@
 package com.artniewski.scoreboard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ScoreBoard {
+
+    private final Map<String, Match> matches = new HashMap<>();
 
     public Match startGame(String homeTeamName, String awayTeamName) {
         validateTeamNames(homeTeamName, awayTeamName);
-        return new Match(homeTeamName, awayTeamName);
+        Match match = new Match(homeTeamName, awayTeamName);
+        matches.values()
+                .stream()
+                .filter(m -> m.getHomeTeam().equalsIgnoreCase(homeTeamName)
+                        || m.getAwayTeam().equalsIgnoreCase(homeTeamName)
+                        || m.getHomeTeam().equalsIgnoreCase(awayTeamName)
+                        || m.getAwayTeam().equalsIgnoreCase(awayTeamName))
+                .findFirst()
+                .ifPresent(m -> {
+                    throw new GameException("Cannot start a game with a team already in play");
+                });
+        String matchId = homeTeamName.toLowerCase() + "#" + awayTeamName.toLowerCase();
+        matches.put(matchId, match);
+        return match;
     }
 
     private static void validateTeamNames(String homeTeamName, String awayTeamName) {

283b84a42e7c6242411c31fa3449078f0e1af08f
a.trzesniewski <artniewski@gmail.com>
2025-04-25 19:58:33 +0200

    introduce nested classes in ScoreBoardTest

--- src/test/java/com/artniewski/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/artniewski/scoreboard/ScoreBoardTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -20,160 +22,169 @@ import com.artniewski.scoreboard.exception.GameNotFoundException;
 
 class ScoreBoardTest {
 
-    @Test
-    void shouldStartGameWithInitialScore() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When
-        Match match = scoreBoard.startGame("Mexico", "Canada");
-        // Then
-        assertNotNull(match);
-        assertEquals("Mexico", match.getHomeTeam());
-        assertEquals(0, match.getHomeScore());
-        assertEquals("Canada", match.getAwayTeam());
-        assertEquals(0, match.getAwayScore());
+    static class Teams {
+        public static final String MEXICO = "Mexico";
+        public static final String CANADA = "Canada";
+        public static final String SPAIN = "Spain";
+        public static final String BRAZIL = "Brazil";
+        public static final String GERMANY = "Germany";
+        public static final String FRANCE = "France";
+        public static final String ITALY = "Italy";
+        public static final String ARGENTINA = "Argentina";
     }
 
-    @ParameterizedTest
-    @MethodSource("invalidTeamNames")
-    void shouldThrowExceptionForInvalidTeamNameOnStart(String homeTeamName, String awayTeamName) {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> scoreBoard.startGame(homeTeamName, awayTeamName));
+    ScoreBoard scoreBoard;
+
+    @BeforeEach
+    void setUp() {
+        scoreBoard = new ScoreBoard();
     }
 
-    @Test
-    void shouldThrowGameExceptionForSameTeamNames() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(GameException.class, () -> scoreBoard.startGame("Brazil", "Brazil"));
+    @Nested
+    class StartGameTests {
+        @Test
+        void shouldStartGameWithInitialScore() {
+            // When
+            Match match = scoreBoard.startGame(Teams.MEXICO, Teams.CANADA);
+            // Then
+            assertNotNull(match);
+            assertEquals(Teams.MEXICO, match.getHomeTeam());
+            assertEquals(0, match.getHomeScore());
+            assertEquals(Teams.CANADA, match.getAwayTeam());
+            assertEquals(0, match.getAwayScore());
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.artniewski.scoreboard.ScoreBoardTest#invalidTeamNames")
+        void shouldThrowExceptionForInvalidTeamNameOnStart(String homeTeamName, String awayTeamName) {
+            // When & Then
+            assertThrows(IllegalArgumentException.class, () -> scoreBoard.startGame(homeTeamName, awayTeamName));
+        }
+
+        @Test
+        void shouldThrowGameExceptionForSameTeamNames() {
+            // When & Then
+            assertThrows(GameException.class, () -> scoreBoard.startGame(Teams.BRAZIL, Teams.BRAZIL));
+        }
+
+        @Test
+        void shouldThrowExceptionIfAnyTeamAlreadyInGame() {
+            // Given
+            scoreBoard.startGame(Teams.SPAIN, Teams.ITALY);
+            // When & Then
+            assertThrows(GameException.class, () -> scoreBoard.startGame(Teams.SPAIN, Teams.FRANCE));
+            assertThrows(GameException.class, () -> scoreBoard.startGame(Teams.GERMANY, Teams.ITALY));
+        }
+
+        @Test
+        void shouldIncludeStartedGameInSummary() {
+            // Given
+            Match match = scoreBoard.startGame(Teams.BRAZIL, Teams.ARGENTINA);
+            // When
+            List<Match> summary = scoreBoard.getSummary();
+            // Then
+            assertEquals(1, summary.size());
+            assertIterableEquals(List.of(match), summary);
+        }
     }
 
-    @Test
-    void shouldThrowExceptionIfAnyTeamAlreadyInGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        scoreBoard.startGame("Spain", "Italy");
-        // When & Then
-        assertThrows(GameException.class, () -> scoreBoard.startGame("Spain", "France"));
-        assertThrows(GameException.class, () -> scoreBoard.startGame("Germany", "Italy"));
+    @Nested
+    class FinishGameTests {
+        @Test
+        void shouldThrowExceptionWhenFinishingNonExistentGame() {
+            // When & Then
+            assertThrows(GameNotFoundException.class, () -> scoreBoard.finishGame(Teams.BRAZIL, Teams.ARGENTINA));
+        }
+
+        @Test
+        void shouldSuccessfullyFinishGame() {
+            // Given
+            scoreBoard.startGame(Teams.BRAZIL, Teams.ARGENTINA);
+            // When & Then
+            assertDoesNotThrow(() -> scoreBoard.finishGame(Teams.BRAZIL, Teams.ARGENTINA));
+        }
+
+        @Test
+        void shouldRemoveGameFromSummaryAfterFinish() {
+            // Given
+            scoreBoard.startGame(Teams.BRAZIL, Teams.ARGENTINA);
+            scoreBoard.finishGame(Teams.BRAZIL, Teams.ARGENTINA);
+            // When
+            List<Match> summary = scoreBoard.getSummary();
+            // Then
+            assertTrue(summary.isEmpty());
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.artniewski.scoreboard.ScoreBoardTest#invalidTeamNames")
+        void shouldThrowExceptionForInvalidTeamNameOnFinish(String homeTeamName, String awayTeamName) {
+            // When & Then
+            assertThrows(IllegalArgumentException.class, () -> scoreBoard.finishGame(homeTeamName, awayTeamName));
+        }
     }
 
-    @Test
-    void shouldThrowExceptionWhenFinishingNonExistentGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(GameNotFoundException.class, () -> scoreBoard.finishGame("Brazil", "Argentina"));
+    @Nested
+    class UpdateGameTests {
+        @Test
+        void shouldThrowExceptionUpdatingScoreForNonExistentGame() {
+            // When & Then
+            assertThrows(GameNotFoundException.class, () -> scoreBoard.updateScore(Teams.BRAZIL, Teams.ARGENTINA, 2, 3));
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.artniewski.scoreboard.ScoreBoardTest#invalidTeamNames")
+        void shouldThrowExceptionForInvalidTeamNameOnUpdate(String homeTeamName, String awayTeamName) {
+            // When & Then
+            assertThrows(IllegalArgumentException.class, () -> scoreBoard.updateScore(homeTeamName, awayTeamName, 2, 3));
+        }
+
+        @ParameterizedTest
+        @MethodSource("invalidGameScores")
+        void shouldThrowExceptionUpdatingWithInvalidScore(Integer homeScore, Integer awayScore) {
+            scoreBoard.startGame(Teams.BRAZIL, Teams.ARGENTINA);
+            // When & Then
+            assertThrows(IllegalArgumentException.class, () -> scoreBoard.updateScore(Teams.BRAZIL, Teams.ARGENTINA, homeScore, awayScore));
+        }
+
+        @Test
+        void shouldUpdateGameScore() {
+            // Given
+            Match match = scoreBoard.startGame(Teams.BRAZIL, Teams.ARGENTINA);
+            // When
+            Match result = scoreBoard.updateScore(Teams.BRAZIL, Teams.ARGENTINA, 2, 3);
+            // Then
+            assertEquals(2, result.getHomeScore());
+            assertEquals(3, result.getAwayScore());
+        }
+
+        private static Stream<Arguments> invalidGameScores() {
+            return Stream.of(
+                    Arguments.of(-3, 0),
+                    Arguments.of(0, -10),
+                    Arguments.of(-1, -1)
+            );
+        }
     }
 
-    @Test
-    void shouldSuccessfullyFinishGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        scoreBoard.startGame("Brazil", "Argentina");
-        // When & Then
-        assertDoesNotThrow(() -> scoreBoard.finishGame("Brazil", "Argentina"));
-    }
+    @Nested
+    class SummaryTests {
 
-    @Test
-    void shouldReturnSummaryWithEmptyList() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When
-        List<Match> summary = scoreBoard.getSummary();
-        // Then
-        assertTrue(summary.isEmpty());
-    }
-
-    @Test
-    void shouldReturnSummaryWithStartedGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        Match match = scoreBoard.startGame("Brazil", "Argentina");
-        // When
-        List<Match> summary = scoreBoard.getSummary();
-        // Then
-        assertEquals(1, summary.size());
-        assertIterableEquals(List.of(match), summary);
-    }
-
-    @Test
-    void shouldReturnEmptySummaryAfterFinishingGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        scoreBoard.startGame("Brazil", "Argentina");
-        scoreBoard.finishGame("Brazil", "Argentina");
-        // When
-        List<Match> summary = scoreBoard.getSummary();
-        // Then
-        assertTrue(summary.isEmpty());
-    }
-
-    @Test
-    void shouldThrowExceptionUpdatingScoreForNonExistentGame() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(GameNotFoundException.class, () -> scoreBoard.updateScore("Brazil", "Argentina", 2, 3));
-    }
-
-    @ParameterizedTest
-    @MethodSource("invalidTeamNames")
-    void shouldThrowExceptionForInvalidTeamNameOnUpdate(String homeTeamName, String awayTeamName) {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> scoreBoard.updateScore(homeTeamName, awayTeamName, 2, 3));
-    }
-
-    @ParameterizedTest
-    @MethodSource("invalidGameScores")
-    void shouldThrowExceptionForInvalidGameScores(Integer homeScore, Integer awayScore) {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        scoreBoard.startGame("Brazil", "Argentina");
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> scoreBoard.updateScore("Brazil", "Argentina", homeScore, awayScore));
-    }
-
-    @Test
-    void shouldUpdateGameScore() {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        Match match = scoreBoard.startGame("Brazil", "Argentina");
-        // When
-        Match result = scoreBoard.updateScore("Brazil", "Argentina", 2, 3);
-        // Then
-        assertEquals(2, result.getHomeScore());
-        assertEquals(3, result.getAwayScore());
-    }
-
-    @ParameterizedTest
-    @MethodSource("invalidTeamNames")
-    void shouldThrowExceptionForInvalidTeamNameOnFinish(String homeTeamName, String awayTeamName) {
-        // Given
-        ScoreBoard scoreBoard = new ScoreBoard();
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () -> scoreBoard.finishGame(homeTeamName, awayTeamName));
+        @Test
+        void shouldReturnSummaryWithEmptyList() {
+            // When
+            List<Match> summary = scoreBoard.getSummary();
+            // Then
+            assertTrue(summary.isEmpty());
+        }
     }
 
     private static Stream<Arguments> invalidTeamNames() {
         return Stream.of(
-                Arguments.of("", "Argentina"),
-                Arguments.of(null, "Argentina"),
-                Arguments.of("Brazil", ""),
-                Arguments.of("Brazil", null)
+                Arguments.of("", Teams.ARGENTINA),
+                Arguments.of(null, Teams.ARGENTINA),
+                Arguments.of(Teams.BRAZIL, ""),
+                Arguments.of(Teams.BRAZIL, null)
         );
     }
 
-    private static Stream<Arguments> invalidGameScores() {
-        return Stream.of(
-                Arguments.of(-3, 0),
-                Arguments.of(0, -10),
-                Arguments.of(-1, -1)
-        );
-    }
 }
